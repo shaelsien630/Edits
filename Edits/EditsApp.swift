e8239c8ac225e50ded1956a98552039bf4efbe6e
shaelsien630 <shaelsien630@gmail.com>
2024-12-18 10:52:53 +0900

    Initial Commit

---
+++ Edits/EditsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditsApp.swift
+//  Edits
+//
+//  Created by 최서희 on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct EditsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
